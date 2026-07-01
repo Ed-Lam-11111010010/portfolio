e4c60d06588d485b99ecdf38e9380788889b0984
Yume Ryusei <30871665+Ed-Lam-11111010010@users.noreply.github.com>
2026-07-02 07:47:24 +0800

    fix: add trailing slash to base URL to fix skill link paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Ed-Lam-11111010010.github.io',
-  base: '/portfolio',
+  base: '/portfolio/',
 });
